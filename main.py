00cb6a982658c8fe3e27f793637f36bd21e0e638
gnbpdx <56661861+gnbpdx@users.noreply.github.com>
2020-05-28 17:05:13 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+
+# Imports
+import os
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from opt import test, training, train
+from feedforward import Net
+def main():
+# Load raw data
+    dataset = 'Arabic_train_test.npz'
+    data = np.load(dataset)
+    X_train = data['X_train']
+    y_train = data['y_train']
+    X_test = data['X_test']
+    y_test = data['y_test']
+    X_train = torch.from_numpy(X_train)
+    y_train = torch.from_numpy(y_train)
+    X_train = X_train.type(torch.FloatTensor)
+    y_train = y_train.type(torch.FloatTensor)
+    X_test = torch.from_numpy(X_test)
+    y_test = torch.from_numpy(y_test)
+    X_test = X_test.type(torch.FloatTensor)
+    y_test = y_test.type(torch.FloatTensor)
+    net = Net()
+    if (torch.cuda.is_available()):
+        net = net.cuda()
+    print(test(X_test, y_test, net))
+    for i in range(500):
+        training(X_train, y_train, net)
+        print(test(X_test, y_test, net))
+
+
+if __name__ == '__main__':
+    main()
+
