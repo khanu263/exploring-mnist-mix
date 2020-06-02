941a4c49fcd302860dceb378ed7c0d36ec39df28
domrany <damon9@pdx.edu>
2020-06-01 21:29:59 -0700

    arbitary output number

--- feedforward.py
+++ feedforward.py
@@ -34,11 +34,11 @@ class FeedForward(nn.Module):
     for example to create a linear model with 3 hidden layers with respectively
     30, 150, 20 hidden nodes in them run the code FeedForward([30,150,20])
     '''
-    def __init__(self, num_hid_nodes: list = [1000,100] ) -> None:
+    def __init__(self, num_hid_nodes: list = [1000,100], outputs: int = 10 ) -> None:
         super(FeedForward,self).__init__() # This is required for initialization
                 
         num_hid_nodes.insert(0,(28*28)) # Add the number of inputs(pixels)
-        num_hid_nodes.append(10) # Add the final number of outputs
+        num_hid_nodes.append(outputs) # Add the final number of outputs
         self.layers = nn.ModuleList()
         for i in range(len(num_hid_nodes)-1):
             self.layers.append(nn.Linear(num_hid_nodes[i], num_hid_nodes[i+1]))
